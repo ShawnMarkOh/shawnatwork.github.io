5ccd80e04e5460c08eaafdfce28ba362ec30fd50
ShawnMarkOh <122619759+ShawnMarkOh@users.noreply.github.com>
2023-07-24 21:26:39 -0400

    Update chat.php

--- chat.php
+++ chat.php
@@ -19,13 +19,11 @@ $message = $_POST['message'];
 // Add the message to the chat history.
 $chat_history = file_get_contents('chat_history.txt');
 $chat_history .= $username . ': ' . $message . "\n";
+chmod('chat_history.txt', 0664);
 file_put_contents('chat_history.txt', $chat_history);
 
 // Redirect to the chat page.
 header('Location: chat.php');
 exit;
-
-// Make the file readable by everyone.
-chmod('chat_history.txt', 0644);
 
 ?>
